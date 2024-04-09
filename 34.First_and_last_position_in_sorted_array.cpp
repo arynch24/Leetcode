739c5a055539910a4397e72a5872599d936d5e51
arynch24 <arynch24@gmail.com>
2024-04-09 18:49:31 +0530

    binary search

---
+++ 34.First_and_last_position_in_sorted_array.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //first
+        int start=0,end=nums.size()-1,mid;
+        int first=-1,last=-1;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                first =mid;
+                end=mid-1;
+            }
+            else if(nums[mid]>target)
+            end=mid-1;
+            else
+            start=mid+1;
+        }
+        //last
+        
+        start=0,end=nums.size()-1;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                last =mid;
+                start=mid+1;
+            }
+            else if(nums[mid]>target)
+            end=mid-1;
+            else
+            start=mid+1;
+        }
+        vector<int>a(2);
+        a[0]=first;
+        a[1]=last;
+        return a;
+    }
+};
