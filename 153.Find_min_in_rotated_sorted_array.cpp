1da01087ee894bd3ab5d521f72e2e48ea76d6a65
arynch24 <arynch24@gmail.com>
2024-04-11 01:42:41 +0530

    Binary Search

---
+++ 153.Find_min_in_rotated_sorted_array.cpp
@@ -0,0 +1,26 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+    int start=0,end=nums.size()-1,mid;
+    int ans=nums[0];
+    while(start<=end)
+    {
+        mid=start+(end-start)/2;
+        //searching right side 
+        if(nums[mid]>=nums[0])
+        start=mid+1;
+        //searching left side
+        else
+        {
+            ans=nums[mid];
+            end=mid-1;
+        }
+    }
+    return ans;
+    }
+};
